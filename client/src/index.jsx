b8fbbfc074edf01bb62bb5b6a608751c4208e23a
Eduardo091 <urrutia.e14@gmail.com>
2018-04-15 19:50:24 -0500

    server is listening on port 3000

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructo(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>hello</div>
+    )
+  }
+}
+ReactDOM.render(<App/>, document.getElementById('app'))
